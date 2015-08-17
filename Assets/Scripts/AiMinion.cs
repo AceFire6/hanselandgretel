7d666395a53c856ea69b26b31e77e922929f6679
Muhummad Patel <muhummad.patel@gmail.com>
2015-08-17 12:40:12 +0200

    Add wander state to minion.
    The minion now has a wander state. The movement script and minionAI
    scripts were written to facilitate this behaviour. Still need to write
    the code for the chase and attack states.

---
+++ Assets/Scripts/AiMinion.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AiMinion : MonoBehaviour {
+
+	Movement movement;
+
+	enum State {Wandering, Chasing, Attacking};
+	State current;
+	private GameObject[] players;
+	float chaseRadius = 6.0f;
+	float attackRadius = 3.0f;
+	float wanderTime = 2.0f;
+	float timeWandering = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		movement = (Movement)GetComponent<Movement>();
+		current = State.Wandering;
+		players = GameObject.FindGameObjectsWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		UpdateState();
+		ExecuteState();
+	}
+
+	void UpdateState(){
+		Vector3 minDist = transform.position - players[0].transform.position;
+//		Debug.Log (minDist);
+		foreach (GameObject player in players){
+			if((transform.position - player.transform.position).sqrMagnitude < minDist.sqrMagnitude){
+				minDist = transform.position - player.transform.position;
+			}
+		}
+
+		float closest = minDist.sqrMagnitude;
+
+		if (closest < attackRadius){
+			current = State.Attacking;
+		}else if (closest < chaseRadius){
+			current = State.Chasing;
+		}else{
+			current = State.Wandering;
+		}
+	}
+
+	void ExecuteState(){
+		switch (current){
+			case State.Wandering:
+				Wander();
+				break;
+			case State.Chasing:
+				Chase();
+				break;
+			case State.Attacking:
+				Attack ();
+				break;
+		}
+	}
+
+	bool dir = false;
+	void Wander(){
+		timeWandering += Time.deltaTime;
+
+		if(timeWandering > wanderTime){
+			timeWandering = 0.0f;
+			movement.SetDeltaRotation(0.0f, 180.0f);
+			dir = !dir;
+		}
+
+		if(dir){
+		movement.SetDeltaMovement(1.0f, 0.0f);
+		}else{
+			movement.SetDeltaMovement(-1.0f, 0.0f);
+		}
+	}
+
+	void Chase(){}
+	void Attack(){}
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public float speed = 3.0f;
+	public float rotSpeed = 250.0f;
+
+	private Vector3 deltaMovement;
+	private Vector3 rotation;
+	private Animator objAnimator;
+	private Rigidbody objRigidbody;
+	
+	void Start () {
+		objAnimator = GetComponent<Animator> ();
+		objRigidbody = GetComponent<Rigidbody> ();
+		rotation = objRigidbody.rotation.eulerAngles;
+	}
+
+	void Update () {
+		//float h = Input.GetAxisRaw ("Horizontal");
+		//float v = Input.GetAxisRaw ("Vertical");
+		
+		Move (deltaMovement.x, deltaMovement.y);
+		Rotate (rotation.x, rotation.y);
+		Animate (deltaMovement.x, deltaMovement.y);
+	}
+
+	public void SetDeltaMovement(float x, float y){
+		deltaMovement = new Vector3(x, y, 0.0f);
+	}
+
+	//todo: make this use a public var as well
+	public void SetDeltaRotation(float x, float y){
+		rotation += new Vector3(x, y, 0.0f);
+
+		if(rotation.x > 360){
+			rotation.x -= 360;
+		}
+		if(rotation.y > 360){
+			rotation.y -= 360;
+		}
+	}
+
+	void Rotate(float x, float y){
+		if(objRigidbody.rotation.eulerAngles != rotation){
+			Quaternion rot = Quaternion.Lerp(objRigidbody.rotation, Quaternion.Euler(rotation), rotSpeed);
+			objRigidbody.MoveRotation ( rot);
+		}
+	}
+
+	void Move(float horizontal, float vertical){
+		deltaMovement.Set (horizontal, vertical, 0.0f);
+		deltaMovement = deltaMovement.normalized * speed * Time.deltaTime;
+
+		objRigidbody.MovePosition (transform.position + deltaMovement);
+	}
+
+	void Animate(float horizontal, float vertical) {
+		objAnimator.SetBool ("isFlying", horizontal != 0);
+	}
+}

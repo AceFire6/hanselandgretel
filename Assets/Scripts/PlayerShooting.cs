ac646b89debf9576092d00de8657dd6603aaa9a8
Aashiq Parker <PRKAAS003@myuct.ac.za>
2015-08-20 23:23:10 +0200

    Synced animation with shooting

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -4,20 +4,34 @@ using System.Collections;
 public class PlayerShooting : MonoBehaviour {
 
 	public Transform arrow;
+
+	private float cooldown;
+	private float timer;
 	private Vector3 positionOffset;
+
+	Animator animator;
 
 	void Start () 
 	{
-		positionOffset = new Vector3 (0, 0.45f, 0);
+		animator = GetComponent<Animator> ();
+		positionOffset = new Vector3 (0, 0.55f, 0);
+		cooldown = 0.292f * 2; //Time it takes to raise and lower bow
 	}
-	
+
+	void Shoot()
+	{
+		positionOffset.x = transform.forward.x * 0.01f;
+		Instantiate (arrow, transform.position + positionOffset,transform.rotation);
+	}
 
 	void Update () 
 	{
-		if (Input.GetKeyDown (KeyCode.M)) 
+		timer += Time.deltaTime;
+		bool canShoot = !animator.GetCurrentAnimatorStateInfo (0).IsName ("Base Layer.Jump") && !animator.GetCurrentAnimatorStateInfo (0).IsName ("Base Layer.Strafe");
+		if (Input.GetKeyDown (KeyCode.Space) && timer > cooldown && canShoot) 
 		{
-			positionOffset.x = transform.forward.x * 0.01f;
-			Instantiate (arrow, transform.position + positionOffset,transform.rotation);
+			timer = 0;
+			Invoke ("Shoot", cooldown /2); //Instantiate after 0.292s when the bow reaches the fire position
 		}
 
 	}

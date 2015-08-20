6b484ce7958d439ca98ca42cc9921c4d12ef1310
Aashiq Parker <PRKAAS003@myuct.ac.za>
2015-08-20 15:30:34 +0200

    Fixed double jumping issue

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,15 @@
-﻿using UnityEngine;
+﻿/*
+ * Script to control Gretel's movement. Inherits from Muhammad's base Movement Script.
+ * Control are: 
+ * 	'A' and 'D' or the arrow keys to move.
+ * 	'W' to jump.
+ * 	Left Shift + Move to strafe.
+ * 	Spacebar to shoot.
+ * 
+ * Author: Aashiq Parker
+ * Date: 20-August-2015
+ */
+using UnityEngine;
 using System.Collections;
 
 public class PlayerMovement : Movement
@@ -11,55 +22,47 @@ public class PlayerMovement : Movement
 	public float strafeSpeed = 0.5f;
 	public float rotationSpeed = 180f;
 	public float jumpSpeed = 200f;
-	bool canJump;
+	public float jumpOffset = 0.3f; //Helps to prevent double jumps, explained below
+	public float timer = 0f;
 
 	void Start () 
 	{
 		base.Start ();
 		base.speed = 1.0f;
 		base.rotSpeed = 2.5f;
-		canJump = true;
 		animator = GetComponent<Animator> ();
 		position = transform.position;
 	}
-
-	void OnCollisionEnter(Collision collision)
-	{
-		canJump = true;
-		//Debug.Log ("Collision");
-	}
-
-	void OnCollisionExit(Collision collision)
-	{
-		canJump = false;
-		//Debug.Log (" No Collision");
-
-	}
-
-	bool IsGrounded()
-	{
-		return Physics.Raycast (transform.position, -Vector3.up, collider.bounds.extents.y + 0.1f);
+	
+	bool IsGrounded() //To check if the player is on the ground so that he can jump again
+	{															//Slight offset for raycast to work	
+		return Physics.Raycast (transform.position + new Vector3(0,0.03f,0), -Vector3.up, collider.bounds.extents.y );
 	}
 
 	void Update()
 	{
 		base.Update ();
-
-		//Debug.Log (animator.GetCurrentAnimatorStateInfo (0).normalizedTime);//.IsName("Base Layer.Idle"));
+		timer += Time.deltaTime;
 		float h = Input.GetAxisRaw ("Horizontal"); //Since we're only moving in this one plane
-		//canJump = !animator.GetCurrentAnimatorStateInfo (0).IsName ("Base Layer.Jump");
-		//Debug.Log (canJump);
 
 		bool running = h != 0;									//Strafing only if shift is pressed, player is running and moving backwards
 		bool strafing = Input.GetKey (KeyCode.LeftShift) && running && transform.forward.x * h < 0;
 		bool shooting = Input.GetKeyDown (KeyCode.Space);
 		bool jumping = Input.GetKeyDown (KeyCode.W) && IsGrounded();// && canJump;
 
-		//Debug.Log (animator.GetCurrentAnimatorStateInfo (0).IsName ("Base Layer.Jump"));
-
-		Debug.Log(Physics.Raycast (transform.position, -Vector3.up, collider.bounds.extents.y + 0.1f));
-		if (jumping) 
+		/* I initially tried using OnCollisionEnter and Exit to set a canJump variable to check if the player was
+		 * on the ground before jumping again to prevent double jumps, but the collisions sometimes don't pickup. I
+		 * suspect it's because the y-pos of the player fluctuates very slightly when walking (why I don't know).
+		 * I then tried using a raycast, which initially didn't work but then I added a slight offset to the y position
+		 * of the player and it did, the problem was that if the player pressed 'w' to jump fast enough then the player
+		 * can still double jump. So what I finally did was at a slight offset and used a timer to disable jumping within
+		 * the first 0.3 seconds of the jump, after which the raycast will fail beacuse the player is in the air.
+		 * */
+		if (jumping && timer > jumpOffset) 
+		{
 			objRigidbody.AddForce (transform.up * jumpSpeed);
+			timer = 0f;
+		}
 
 		animator.SetBool ("isStrafing", strafing);
 		animator.SetBool ("isShooting", shooting);
@@ -80,12 +83,7 @@ public class PlayerMovement : Movement
 			//transform.LookAt(transform.forward*-1); 
 		}
 
-		float v = 0;
-
-		//if (animator.GetCurrentAnimatorStateInfo (0).IsName ("Base Layer.Jump") )//&& animator.GetCurrentAnimatorStateInfo (0).normalizedTime < 0.5)
-	//		v = 10;//objRigidbody.AddForce (transform.up*10);//
-
-		SetDeltaMovement (h, v);
+		SetDeltaMovement (h, 0);
 	}
 
 

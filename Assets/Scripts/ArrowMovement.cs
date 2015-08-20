6672cb9d90ffd5bc91c0f910b01abcaab8560e4b
Aashiq Parker <PRKAAS003@myuct.ac.za>
2015-08-20 16:46:04 +0200

    Created a script for moving the arrow once spawned. The arrow flies until a collision occurs at which it will stick the object it struck

---
+++ Assets/Scripts/ArrowMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowMovement : MonoBehaviour {
+
+	public float speed = 5f;
+	private bool active = true;
+	private float direction;
+
+	Rigidbody body;
+	Vector3 position;
+
+	void Start () 
+	{
+		body = GetComponent<Rigidbody> ();
+		position = transform.position;
+		direction = transform.forward.x; 
+	}
+
+	void Update () 
+	{
+		if (active) 
+		{	
+			position.x += speed * Time.deltaTime * direction;
+			body.MovePosition (position);
+		}
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		Debug.Log ("Collision");
+		body.isKinematic = true;
+		active = false;
+		transform.parent = collision.transform;
+	}
+}

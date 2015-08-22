6723d3e820dcf2d36ba58d34ddb26b0475fe0990
Aashiq Parker <PRKAAS003@myuct.ac.za>
2015-08-22 13:12:54 +0200

    Created game manager script and game manager object that has 4 spawn points (at different locations) as children

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * GameManager script to control the spawning of AI at different spawn points and update the amount 
+ * of coins the player possesses.
+ * 
+ * Author: Aashiq Parker
+ * Date: 22-August-2015
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	
+	void Start () 
+	{
+	
+	}
+
+	void Update () 
+	{
+	
+	}
+}

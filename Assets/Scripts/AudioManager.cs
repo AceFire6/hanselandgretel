3eff7e0e09078dfc41f657960913584eccbd0012
Muhummad Patel <muhummad.patel@gmail.com>
2015-09-16 18:49:47 +0200

    Add AudioManager script to keep track of audio related variables.

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour {
+
+	AudioSource backgroundMusic;
+
+	private float musicVolume = 1.0f;
+	private float soundVolume = 1.0f;
+
+	private bool isMusicMute = false;
+	private bool isSoundMute = false;
+
+	void Start () {
+		backgroundMusic = GetComponent<AudioSource> ();
+	}
+
+	void Update () {
+	
+	}
+
+	public void AdjustMusicVolume (float vol){
+		vol = Mathf.Clamp01 (vol);
+
+		backgroundMusic.volume = vol;
+	}
+
+	public void ToggleMusicMute (bool isMute) {
+		isMusicMute = isMute;
+		backgroundMusic.mute = isMute;
+	}
+}

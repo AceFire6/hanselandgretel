636459a20a60de85b9a3d70f49a3a6cb29a0acb7
Muhummad Patel <muhummad.patel@gmail.com>
2015-09-16 19:07:21 +0200

    Document AudioManager script.

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,11 +1,19 @@
-﻿
+﻿/*
+ * AudioManager script handles all sound related operations. It stores the nackgroundMusic
+ * AudioSource, and adusts the music and sound volumes. It also handles muting the music
+ * or sound effects. The adjustment methods are event callbacks for the settings menu GUI
+ * elements (sound and music sliders and mute checkboxes).
+ * 
+ * Author: Muhummad Patel
+ * Date: 15-September-2015
+ */
 
 using UnityEngine;
 using System.Collections;
 
 public class AudioManager : MonoBehaviour {
 
-	AudioSource backgroundMusic;
+	private AudioSource backgroundMusic;
 
 	private float musicVolume = 1.0f;
 	private float soundVolume = 1.0f;
@@ -15,6 +23,8 @@ public class AudioManager : MonoBehaviour {
 
 	void Start () {
 		backgroundMusic = GetComponent<AudioSource> ();
+
+		//TODO: Get and store array of all AudioSources in the scene (Except background music)
 	}
 
 	public void AdjustMusicVolume (float vol){
@@ -30,9 +40,11 @@ public class AudioManager : MonoBehaviour {
 
 	public void AdjustSoundVolume (float vol){
 		soundVolume = Mathf.Clamp01 (vol);
+		//TODO: adjust volume of all audioSources
 	}
 	
 	public void ToggleSoundMute (bool isMute) {
 		isSoundMute = isMute;
+		//TODO: mute all audioSources
 	}
 }

ff004a4763b6628b6e218380cf2184a5d9203b9e
muhummadPatel <muhummad.patel@gmail.com>
2015-10-10 16:03:02 +0200

    Make the camera zoom out when players enter the boss fight area so that we can see the whole boss model nicely.

--- Assets/Scripts/RaiseFence.cs
+++ Assets/Scripts/RaiseFence.cs
@@ -6,6 +6,8 @@ public class RaiseFence : MonoBehaviour {
 	public float raiseSpeed = 0.7f; //speed at which the fence will rise
 	public float depth = 0.5f; //How far to sink/rise
 	public float rightTriggerDist = 0.7f; //How far must the players have walked past us to trigger the gate to rise
+	public float cameraZoomedOutZ = -5f;
+	public float cameraZoomSpeed = 0.7f;
 
 	private bool isTriggered = false;
 	private Rigidbody rigidbody;
@@ -30,14 +32,23 @@ public class RaiseFence : MonoBehaviour {
 		Vector3 pos = transform.position;
 		pos.y -= depth;
 		transform.position = pos;
+
 	}
 
 	void Update () {
-		//if are triggered and aren't yet at the up position
-		if (transform.position.y < upPos.y && isTriggered) {
-			Vector3 pos = transform.position;
-			pos.y += (raiseSpeed * Time.deltaTime);
-			transform.position = pos;
+		if (isTriggered){
+			//if are triggered and aren't yet at the up position
+			if (transform.position.y < upPos.y) {
+				Vector3 pos = transform.position;
+				pos.y += (raiseSpeed * Time.deltaTime);
+				transform.position = pos;
+			}
+
+			if (Camera.main.transform.position.z > cameraZoomedOutZ){
+				Vector3 camPos = Camera.main.transform.position;
+				camPos.z = Mathf.Lerp(camPos.z, cameraZoomedOutZ, cameraZoomSpeed * Time.deltaTime);
+				Camera.main.transform.position = camPos;
+			}
 		}
 	}
 
@@ -53,7 +64,7 @@ public class RaiseFence : MonoBehaviour {
 
 
 	void OnTriggerExit(Collider other) {
-		if(other.tag == "Player"){
+		if(other.tag == "Player" && !isTriggered){
 			Debug.Log ("PlayerTrigger " + other.name + " "+ AllPlayersPassed());
 			if (AllPlayersPassed ()) {
 				Trigger();

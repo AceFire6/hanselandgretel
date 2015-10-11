ac8594d084e261e1196a3f306f3df8902b75e449
muhummadPatel <muhummad.patel@gmail.com>
2015-10-11 12:23:30 +0200

    Witch now flies ahead of players and bobs up and down.

---
+++ Assets/Scripts/WitchController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WitchController : MonoBehaviour {
+
+	public float rightMargin = 0.5f; //How far from the right edge of the screen the witch will fly
+
+	private Camera cam;
+	private float depth;
+
+	void Start () {
+		cam = Camera.main;
+		depth = transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		//Work out where the witch should be relative to the camera
+		float rightBorderPos = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, depth)).x;
+		float xPos = rightBorderPos - rightMargin;
+
+		//make sure the witch is only moving forward
+		float oldX = transform.position.x;
+		xPos = Mathf.Max (xPos, oldX);
+
+		//set the Witch's xPos to the newly calculated x position.
+		Vector3 pos = transform.position;
+		pos.x = xPos;
+		transform.position = pos;
+	}
+}
